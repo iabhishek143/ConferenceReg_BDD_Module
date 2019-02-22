ed40eef649fc2197516e0d7560188637b0f51a88
iabhishek143 <43550368+iabhishek143@users.noreply.github.com>
2019-02-22 17:04:35 +0530

    Add files via upload

---
+++ 168344_ConferenceReg _SetA/src/com/cg/conferencereg/TestRunner.java
@@ -0,0 +1,10 @@
+package com.cg.conferencereg;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="features")
+public class TestRunner {	}
